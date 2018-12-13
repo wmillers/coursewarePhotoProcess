f3cf8c557e55be0cd9677d1a82e7c556dcb95392
woshimaj <ianswiftcircle@gmail.com>
2018-12-13 16:24:38 +0800

    solve part of the opencv2 not supporting unicode problem

--- image_p.py
+++ image_p.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 from toolkit import *
-from scipy import ndimage
 
 dc=True  # Debug Code
 dc=False
@@ -13,52 +12,79 @@ therefore I can finish the project.And also thanks the help
 from stackoverflow.com for solving some of the problems.
 '''
 
+def loadImgUnicode(img_name=r'im\1.jpg'):
+    '''
+    Default
+    '''
+    if dc:print(type(img_name))
+    stream = open(img_name, "rb")
+    bytes = bytearray(stream.read())
+    nparray = np.asarray(bytes, dtype=np.uint8)
+    return cv2.imdecode(nparray, cv2.IMREAD_UNCHANGED)
+
+def loadImgCompatible(img_name=r'im\1.jpg'):
+    '''
+    Not support whitespaces and unicode characters
+    in file name, but it is much more compatible.
+    '''
+    return cv2.imread(img_name)
+
+def writeImg(file_path, dst):
+    '''
+    解决了opencv2处理文件名unicode的问题
+    但是路径还是不能带空格和unicode
+    '''
+    file_path=file_path.encode('ascii', 'ignore').decode('ascii')
+    if not cv2.imwrite(file_path, dst):raise UnicodeTranslateError
+
+
+'''旋转图片，（暂时）仅支持90度的倍数旋转'''
+def rotateProperly(img, angle=0):
+    return np.rot90(img, -int(angle/90))
+
 
 '''
-Part 1
-
-1) 对ppt区域进行识别、还原原文变形
+对ppt区域进行识别、还原原文变形
 '''
 
-def stretchProperly(img_name=r'im\1.jpg', max_size=1200):
-
-    # load image
-    img = cv2.imread(img_name)
+def stretchProperly(img, max_size=1200):
     img_ratio, rsz_img = cv_resize(img,max_size)  # resize since image is huge
     #rsz_img=img # Block the change of resizing image
     gray = cv2.cvtColor(rsz_img, cv2.COLOR_BGR2GRAY)  # convert to grayscale
 
-    # threshold to get just the signature
+    # threshold
     retval, thresh_gray = cv2.threshold(gray, thresh=140, maxval=255, type=cv2.THRESH_BINARY)
 
     im2, contours, hierarchy = cv2.findContours(thresh_gray,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
     # 寻找面积最大的区域
-    max_contour= max(contours, key= cv2.contourArea)
+    max_contour= max(contours, key=lambda x:abs(cv2.contourArea(x)))
 
     # 绘制近似四边形
     # 不能使用minAreaRect，其返回值是最小面积的矩形，
     # 与要求的四边形不符
-    # cv2.drawContours(rsz_img, cv_BoxPoints(cv2.minAreaRect(max_contour)), -1, (0,255,0), 2)
     approx_points=cv2.approxPolyDP(max_contour,0.1 * cv2.arcLength(max_contour, True),True)
 
-
-    if dc:prints(approx_points, np.asarray(corner_points(approx_points)))
+    if dc:
+        con_img=np.copy(rsz_img)
+        cv2.drawContours(con_img, contours, -1, (0,255,255), 3)
+        cv2.drawContours(con_img, cv_BoxPoints(cv2.minAreaRect(max_contour)), -1, (0,255,0), 2)
+        # cv2.drawContours(con_img, approx_points, -1, (255,0,0), 1)
+        plt_show(con_img)
+        del con_img
     # 计算拉伸后的矩形定位点并图形变换
     M= cv2.getPerspectiveTransform(np.asarray(approx_points, np.float32),
-                                         np.asarray(corner_points(approx_points), np.float32))
+                                   np.asarray(corner_points(approx_points), np.float32))
     dst= cv2.warpPerspective(rsz_img, M,
-                                   (corner_points(approx_points)[2][0][0],
-                                    corner_points(approx_points)[1][0][1]))
+                             (corner_points(approx_points)[2][0][0],
+                              corner_points(approx_points)[1][0][1]))
 
     if dc:plt_show(rsz_img, dst)
     return dst
 
 '''
-Part 2
-
-2) 对指定区域（尽量保持纯色背景）的内容清晰化、二值化处理
-   并对其中带图形的区域尽最大可能保留信息量
+对指定区域（尽量保持纯色背景）的内容清晰化、二值化处理
+并对其中带图形的区域尽最大可能保留信息量
 '''
 def threshProperly(img, limit=0.1, area=2):
     '''
@@ -95,7 +121,7 @@ def threshProperly(img, limit=0.1, area=2):
     corner_mean=[]
     j=0
     #150~190
-    for i in range(130,210,10):
+    for i in list(range(130,190,10))+list(range(190,230,5)):
         retval, temp = cv2.threshold(gray, thresh=i, maxval=white, type=cv2.THRESH_BINARY)
         thresh_gray.append(temp)
         corner_area.append(np.ma.array(thresh_gray[j],mask=outside_ellipse))
@@ -105,24 +131,21 @@ def threshProperly(img, limit=0.1, area=2):
             del corner_mean[j]
             break
         j+=1
+    if dc:plt_show(*thresh_gray[-4:])
     corner_mean=np.array(corner_mean)
     corner_baseline=corner_mean.max()-(corner_mean.max()-corner_mean.min())*limit
-    #尽量保持最多的信息量
+    corner_baseline=0.85 if corner_baseline>0.85 else corner_baseline    #尽量保持最多的信息量
     for i in range(0,len(corner_mean)):
         if(corner_mean[i]<corner_baseline):
             if(i>0):
                 i-=1
             break
-    return thresh_gray[i]
     '''
     corner_mean_k=[corner_mean[k]>=corner_baseline for k in range(0,len(corner_mean))]
-
     plt_dotshow(corner_mean)
     print(corner_mean_k)
     cv_show(*corner_area)'''
-
-
-
+    return thresh_gray[i]
 
 
 '''
@@ -137,10 +160,8 @@ def removeNoiseImg(img):
     return img
 
 '''
-Part 3
-
-3) 文件处理模块，遍历指定根目录，并将文件经过p1、p2处理，
-   保存至指定文件夹（默认根目录下的output文件夹）
+文件处理模块，遍历指定根目录，并将文件经过p1、p2处理，
+保存至指定文件夹（默认根目录下的output文件夹）
 '''
 
 
