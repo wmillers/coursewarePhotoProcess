e531a2266a03b41eeb4d336451c855fb5da70083
woshimaj <ianswiftcircle@gmail.com>
2018-12-16 00:01:04 +0800

    quick test for a single file

--- example.py
+++ example.py
@@ -1,12 +1,23 @@
-import file_p
-from toolkit import errorProcess
+import file_p, image_p
+from toolkit import errorProcess,plt_show,cv_show
 
+'''
+快速测试单张图片
+'''
+filePath='C:\\Users\\Administrator\\Desktop\\m\\5.jpg'
+file=filePath
+#image_p.dc=True
 # file_p.getExifOrientation()
 
-a=errorProcess(debug=True)
-for i in range(0,3):
-    a.add_show(i,'',FileExistsError)
-print('===')
-a.show_all_type()
-exit(a.error_code())
-#print(np.array([1,1])*5)
+
+img = image_p.loadImgUnicode(filePath)
+
+angle = file_p.getExifOrientation(filePath)
+if angle != 0:
+    dst = image_p.rotateProperly(img, angle)
+else:
+    dst=img
+dst1 = image_p.stretchProperly(dst)
+dst2 = image_p.threshProperly(dst1)
+# image_p.writeImg('C:\\Users\\Administrator\\Desktop\\OutPUTa.jpg',dst2)
+plt_show(img,dst,dst1)
